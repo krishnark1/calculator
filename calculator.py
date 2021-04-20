a7cbfdccd375b6292582906fef11c3b5a9d6f12c
krishnark1 <krishnark1000@gmail.com>
2021-04-20 21:09:59 +0530

    code is created

--- calculator.py
+++ calculator.py
@@ -1,1 +1,19 @@
-
+while True:
+    a=int(input('enter a number'))
+    b=int(input('enter a number'))
+    i=(input('enter +\-\*\/\%'))
+    if i=='+':
+        print(a+b)
+    elif i=='-':
+        print(a-b)
+    elif i=='*':
+        print(a*b)
+    elif i=='/':
+        print(a/b)
+    elif i=='%':
+        print(a%b)
+    n=input("enter y to continue and q to exit ")
+    if n=='y':
+        continue
+    elif n=='q':
+        break
